fbf2b3688aad7fb62434a25ae9da678435320a90
Valentyn Saik <svbackend22@gmail.com>
2020-06-17 12:06:46 +0200

    Some adjusments for UI

--- app/src/main/java/com/svbackend/cropofil/ui/home/HomeFragment.kt
+++ app/src/main/java/com/svbackend/cropofil/ui/home/HomeFragment.kt
@@ -1,36 +1,26 @@
 package com.svbackend.cropofil.ui.home
 
-import android.Manifest
-import android.app.Activity
 import android.app.DownloadManager
 import android.content.*
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.provider.BaseColumns
-import android.provider.MediaStore
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.net.toFile
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.svbackend.cropofil.R
 import com.svbackend.cropofil.ui.ScopedFragment
-import java.io.DataInputStream
-import java.io.DataOutputStream
+import kotlinx.android.synthetic.main.fragment_home.*
 import java.io.File
-import java.io.FileOutputStream
-import java.net.URL
-import java.net.URLConnection
 import java.util.zip.ZipFile
 
 
@@ -45,37 +35,50 @@ class HomeFragment : ScopedFragment() {
     ): View? {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
 
-        val url = activity?.intent?.data;
-        homeViewModel.setUrl(url.toString())
-
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val title: TextView = root.findViewById(R.id.title_text)
+        homeViewModel.title.observe(viewLifecycleOwner, Observer {
+            title.text = it
         })
 
+        val subtitle: TextView = root.findViewById(R.id.subtitle_text)
+        homeViewModel.subtitle.observe(viewLifecycleOwner, Observer {
+            subtitle.text = it
+        })
+
+        val url: EditText = root.findViewById(R.id.url_edit_text)
+        homeViewModel.url.observe(viewLifecycleOwner, Observer {
+            url.setText(it)
+        })
+
+        val uri = activity?.intent?.data;
+        homeViewModel.setUrl(uri)
+
         val downloadBtn = root.findViewById<Button>(R.id.download_btn);
-        downloadBtn.setOnClickListener { onClickOnDownloadBtn() };
-
-
+        downloadBtn.setOnClickListener { startDownloading() };
 
         return root
     }
 
-    private fun onClickOnDownloadBtn() {
-        val downloadUrl = "${homeViewModel.text.value}/download"
+    private fun startDownloading() {
+        val downloadUrl = "${url_edit_text.text}/download"
+
+        if (!URLUtil.isNetworkUrl(downloadUrl)) {
+            Toast.makeText(context, "$downloadUrl is not a valid url", Toast.LENGTH_LONG).show()
+            return
+        }
+
         Toast.makeText(context, "Downloading started.. Please wait", Toast.LENGTH_LONG).show()
         //val galleryId = homeViewModel.text.value!!.split("/").last()
 
         val request = DownloadManager
-            .Request (Uri.parse(downloadUrl))
+            .Request(Uri.parse(downloadUrl))
             .setTitle("cropofil.zip")
             .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "cropofil.zip")
             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION)
 
         val dm = activity!!.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
         val downloadId = dm.enqueue(request)
-
 
         val receiver = object:BroadcastReceiver() {
             override fun onReceive(p0: Context?, p1: Intent?) {
@@ -109,9 +112,14 @@ class HomeFragment : ScopedFragment() {
         activity!!.registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
+    private fun unzipGallery(zipFile: String) {
+        unzipGallery(File(zipFile))
+    }
+
     private fun unzipGallery(zipFile: File) {
         ZipFile(zipFile).use { zip ->
             zip.entries().asSequence().forEach { entry ->
+                println(entry.name)
                 zip.getInputStream(entry).use { input ->
                     File(entry.name).outputStream().use { output ->
                         input.copyTo(output)
--- app/src/main/java/com/svbackend/cropofil/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/svbackend/cropofil/ui/home/HomeViewModel.kt
@@ -1,21 +1,39 @@
 package com.svbackend.cropofil.ui.home
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+    private val _title = MutableLiveData<String>().apply {
+        value = "Cropofil"
+    }
+    val title: LiveData<String> = _title
 
-    private val _text = MutableLiveData<String>().apply {
+    private val _subtitle = MutableLiveData<String>().apply {
+        value = "Share photos in original quality and resolution"
+    }
+    val subtitle: LiveData<String> = _subtitle
+
+    private val _url = MutableLiveData<String>().apply {
         value = ""
     }
-    val text: LiveData<String> = _text
+    val url: LiveData<String> = _url
+
+    fun setUrl(url: Uri?) {
+        if (url == null) {
+            return
+        }
+
+        setUrl(url.toString())
+    }
 
     fun setUrl(url: String?) {
         if (url == null) {
             return
         }
 
-        _text.apply { value = url }
+        _url.apply { value = url }
     }
 }
