d9f27a78d51adee75402bd749a0677b6557d765f
Valentyn Saik <svbackend22@gmail.com>
2020-06-17 19:21:33 +0200

    Added pre-release icons and fixes

---
+++ app/src/main/java/com/svbackend/cropofil/data/Photo.kt
@@ -0,0 +1,6 @@
+package com.svbackend.cropofil.data
+
+data class Photo (
+    val url: String,
+    val clientFilename: String
+)
--- app/src/main/java/com/svbackend/cropofil/ui/home/HomeFragment.kt
+++ app/src/main/java/com/svbackend/cropofil/ui/home/HomeFragment.kt
@@ -1,11 +1,11 @@
 package com.svbackend.cropofil.ui.home
 
 import android.app.DownloadManager
-import android.content.*
+import android.content.Context
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Environment
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,14 +14,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.net.toFile
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.svbackend.cropofil.R
+import com.svbackend.cropofil.data.Photo
 import com.svbackend.cropofil.ui.ScopedFragment
 import kotlinx.android.synthetic.main.fragment_home.*
+import org.json.JSONObject
 import java.io.File
-import java.util.zip.ZipFile
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class HomeFragment : ScopedFragment() {
@@ -55,76 +57,59 @@ class HomeFragment : ScopedFragment() {
         homeViewModel.setUrl(uri)
 
         val downloadBtn = root.findViewById<Button>(R.id.download_btn);
-        downloadBtn.setOnClickListener { startDownloading() };
+        downloadBtn.setOnClickListener {
+            val galleryUrl = url_edit_text.text.toString();
+            if (!URLUtil.isNetworkUrl(galleryUrl)) {
+                Toast.makeText(context, "$galleryUrl is not a valid url", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            val galleryShortcut = galleryUrl.split("/g/").last()
+            val photosUrl = "https://cropofil.com/api/g/$galleryShortcut"
+            FetchGalleryTask().execute(photosUrl)
+        };
 
         return root
     }
 
-    private fun startDownloading() {
-        val downloadUrl = "${url_edit_text.text}/download"
+    private fun startDownloading(photo: Photo) {
+        val downloadToDir = File(Environment.DIRECTORY_PICTURES.toString() + File.separator + "Cropofil");
 
-        if (!URLUtil.isNetworkUrl(downloadUrl)) {
-            Toast.makeText(context, "$downloadUrl is not a valid url", Toast.LENGTH_LONG).show()
-            return
+        if (!downloadToDir.exists()) {
+            downloadToDir.mkdirs()
         }
-
-        Toast.makeText(context, "Downloading started.. Please wait", Toast.LENGTH_LONG).show()
-        //val galleryId = homeViewModel.text.value!!.split("/").last()
 
         val request = DownloadManager
-            .Request(Uri.parse(downloadUrl))
-            .setTitle("cropofil.zip")
-            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "cropofil.zip")
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION)
+            .Request(Uri.parse(photo.url))
+            .setTitle(photo.clientFilename)
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, "Cropofil${File.separator}${photo.clientFilename}")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
 
         val dm = activity!!.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        val downloadId = dm.enqueue(request)
+        dm.enqueue(request)
+    }
 
-        val receiver = object:BroadcastReceiver() {
-            override fun onReceive(p0: Context?, p1: Intent?) {
-                val id = p1?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-                if (id == downloadId) {
-                    val cursor = dm.query(DownloadManager.Query().setFilterById(id));
-                    cursor.moveToFirst();
-
-                    var downloadedTo = Uri.parse(cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI)))
-
-                    val file = downloadedTo.toFile()
-
-                    println("=====CHECKS:=======")
-                    println(file.isFile)
-                    println(file.exists())
-                    println(file.canRead())
-                    println(file.setReadable(true))
-
-                    try {
-                        unzipGallery(file)
-                        //downloadGallery(downloadUrl, downloadedFile)
-                    } catch (e: Throwable) {
-                        println("========ERROR=======")
-                        e.printStackTrace()
-                        println("========ERROR=======")
-                        Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
+    inner class FetchGalleryTask : AsyncTask<String, String, Unit>() {
+        override fun doInBackground(vararg url: String?) {
+            val json : String;
+            val galleryUrl = url[0];
+            val conn = URL(galleryUrl).openConnection() as HttpURLConnection
+            try {
+                conn.connect()
+                json = conn.inputStream.use { it.reader().use { reader -> reader.readText() } }
+            } finally {
+                conn.disconnect()
             }
-        }
-        activity!!.registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-    }
 
-    private fun unzipGallery(zipFile: String) {
-        unzipGallery(File(zipFile))
-    }
+            val jsonResponse = JSONObject(json)
+            val photos = jsonResponse.getJSONArray("photos")
 
-    private fun unzipGallery(zipFile: File) {
-        ZipFile(zipFile).use { zip ->
-            zip.entries().asSequence().forEach { entry ->
-                println(entry.name)
-                zip.getInputStream(entry).use { input ->
-                    File(entry.name).outputStream().use { output ->
-                        input.copyTo(output)
-                    }
-                }
+            for(i in 0 until photos.length()) {
+                val photo = photos.getJSONObject(i)
+                startDownloading(Photo(
+                    photo.getString("url"),
+                    photo.getString("client_filename")
+                ))
             }
         }
     }
